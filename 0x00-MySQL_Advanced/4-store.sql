4eebc6c7dc0d7f3d4a7b8875c1d1bdd4dd9233d3
antoineleno <johndoe@example.com>
2024-10-17 03:51:44 +0800

    MySQL Advanced

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,13 @@
---- Create a trigger to decrease a quantity
---- When a new order is added
-DROP TRIGGER IF EXISTS decrease_quantity;
-delimiter #
-CREATE TRIGGER decrease_quantity AFTER INSERT ON orders FOR EACH ROW BEGIN UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name; end #
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
 DELIMITER ;
