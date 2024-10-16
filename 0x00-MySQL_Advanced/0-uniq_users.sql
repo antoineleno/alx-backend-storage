4a852879e0f13b9fbbf831d51aeb4894154b0643
antoineleno <johndoe@example.com>
2024-10-17 01:32:36 +0800

    Mysql advanced

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,4 +6,8 @@
 --- If the table already exists, your script should not fail
 --- Your script can be executed on any database
 
-CREATE TABLE IF NOT EXISTS users (id integer AUTO_INCREMENT, email varchar(255) not null unique, name varchar(255));
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
