a59b2d2627e2e906a7fa83cb994b4204e6e9f7f5
antoineleno <johndoe@example.com>
2024-10-17 16:56:30 +0800

    MySQL Advanced

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,17 +1,15 @@
 -- Create function
 -- Check if the second number is 0
-DELIMITER //
-
 DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 2)
-DETERMINISTIC
+DELIMITER //
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN a / b;
-    END IF;
-END //
+    DECLARE result FLOAT DEFAULT 0;
 
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END //
 DELIMITER ;
