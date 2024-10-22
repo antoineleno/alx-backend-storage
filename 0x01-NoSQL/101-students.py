8dda73113abb6b932656109f67212503e04736f8
antoineleno <johndoe@example.com>
2024-10-22 22:26:34 +0800

    NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" 100 students modeule"""
+
+
+def top_students(mongo_collection):
+    """Top student"""
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ]
+    return list(mongo_collection.aggregate(pipeline))
